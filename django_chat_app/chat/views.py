1e2e301e83bab2e4665b8acf10e1590706aa437a
MustafaGuer <mustafaguer@outlook.com>
2022-09-04 00:37:04 +0200

    add: auth guard

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from chat.models import Chat, Message
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/login/')
 def index(request):
     if (request.method == 'POST'):
         print('Data received ' + request.POST['themessage'])
@@ -15,15 +17,15 @@ def index(request):
 
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
-
 def login_view(request):
+    redirect = request.GET.get('next')
     if (request.method == 'POST'):
         user = authenticate(username=request.POST.get(
             'username'), password=request.POST.get('password'))
         if user:
             login(request, user)
-            return HttpResponseRedirect('/chat/')
+            return HttpResponseRedirect(request.POST.get('redirect'))
         else:
-            return render(request, 'auth/login.html', {'wrongPassword': True})
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
 
-    return render(request, 'auth/login.html')
+    return render(request, 'auth/login.html', {'redirect': redirect})
