c8e037d20358744cafacefd7c5cbd248e02bb260
MustafaGuer <mustafaguer@outlook.com>
2022-09-03 22:23:37 +0200

    add: auth check

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from chat.models import Chat, Message
+
+
 def index(request):
-    if(request.method == 'POST'):
+    if (request.method == 'POST'):
         print('Data received ' + request.POST['themessage'])
-    return render(request, 'chat/index.html', {'name': 'MusG'})
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(
+            text=request.POST['themessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+        
+    return render(request, 'chat/index.html', {'messages': chatMessages})
