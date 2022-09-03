aca985c46fcbb8a9bce67744f4cd8f9b8dbdb6a3
MustafaGuer <mustafaguer@outlook.com>
2022-09-03 23:51:13 +0200

    add: login view

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from chat.models import Chat, Message
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
 
 def index(request):
@@ -10,5 +12,18 @@ def index(request):
         Message.objects.create(
             text=request.POST['themessage'], chat=myChat, author=request.user, receiver=request.user)
     chatMessages = Message.objects.filter(chat__id=1)
-        
+
     return render(request, 'chat/index.html', {'messages': chatMessages})
+
+
+def login_view(request):
+    if (request.method == 'POST'):
+        user = authenticate(username=request.POST.get(
+            'username'), password=request.POST.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect('/chat/')
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True})
+
+    return render(request, 'auth/login.html')
