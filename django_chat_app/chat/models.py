329a99d84a5eede9e30b07222ebce676e391a53e
MustafaGuer <mustafaguer@outlook.com>
2022-09-03 21:26:18 +0200

    add: chat class

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -1,13 +1,19 @@
+from email import message
 from django.db import models
-from django.db.models.fields import DateField
 from datetime import date
 from django.conf import settings
 
 
+class Chat(models.Model):
+    created_at = models.DateField(default=date.today)
+
+
 class Message(models.Model):
     text = models.CharField(max_length=500)
-    created_at = DateField(default=date.today)
+    created_at = models.DateField(default=date.today)
     # chat = Chat Klasse verknüpfen
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE,
+                             related_name='chat_message_set', default=None, blank=True, null=True)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(
--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
+    if(request.method == 'POST'):
+        print('Data received ' + request.POST['themessage'])
     return render(request, 'chat/index.html', {'name': 'MusG'})
